a61516e2c878fbb5cbe8463746b4c8625c2966e7
tsueken <tsueken@gmail.com>
2020-02-25 15:28:30 +0900

    メッセージ送信機能の実装

--- chat-space/app/controllers/groups_controller.rb
+++ chat-space/app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+
   def index
   end
 
@@ -6,6 +7,7 @@ class GroupsController < ApplicationController
     @group = Group.new
     @group.users << current_user
   end
+
   def create
     @group = Group.new(group_params)
     if @group.save
@@ -18,19 +20,19 @@ class GroupsController < ApplicationController
   def edit
     @group = Group.find(params[:id])
   end
-  
+
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: "グループを編集しました"
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
-
   end
 
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [])
+    params.require(:group).permit(:name, user_ids: [] )
   end
+
 end
